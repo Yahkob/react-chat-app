e54f347845c1694d9f363081ddf2c7939549b877
Yahkob <JakobHarclerode@gmail.com>
2016-07-21 00:55:24 -0700

    add webpack.loaders export, add empty app.html, finish webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,37 @@
+var webpack = require('webpack');
+var path = require('path');
+var loaders = require('./webpack.loaders');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const HOST = process.env.HOST || "0.0.0.0";
+const PORT = process.env.PORT || "8888";
 module.exports = {
-    entry: './src/app.js',
+    entry: [
+		`webpack-dev-server/client?http://${HOST}:${PORT}`,
+        'webpack/hot/only-dev-server',
+		'./src/app.jsx',
+    ],
     output: {
-        path: './dist',
+        path: path.join(__dirname, 'dist'),
         filename: 'app.bundle.js'
-    }
+    },
+    module: {
+        loaders
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: "./public",
+        noInfo: true,
+        hot: true,
+        inline: true,
+        port: PORT,
+        host: HOST
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin([
+            {from: './app.html'}
+        ]),
+    ]
 };
